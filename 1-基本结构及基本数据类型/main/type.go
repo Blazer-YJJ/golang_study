9be8f534cd855388af8eb23edff042bba98fc626
YangJijun <yangjijuna@gmail.com>
2023-03-15 06:59:25 +0800

    1.3go类型：基本数据类型结构化类型描述类型

--- 1-基本结构及基本数据类型/main/type.go
+++ 1-基本结构及基本数据类型/main/type.go
@@ -1,4 +1,12 @@
 /*
+ * @Author: YangJijun yangjijuna@gmail.com
+ * @Date: 2023-03-14 23:51:26
+ * @LastEditors: YangJijun yangjijuna@gmail.com
+ * @LastEditTime: 2023-03-15 06:58:37
+ * @FilePath: \golang_study\1-基本结构及基本数据类型\main\type.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
  * 类型可以是基本数据类型：
  * int float bool string
  * 结构化的类型：
