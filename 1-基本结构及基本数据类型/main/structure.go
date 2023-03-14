a05daff7c470e8222ee4f52ea5fd0af624b9f203
YangJijun <yangjijuna@gmail.com>
2023-03-15 07:00:21 +0800

    1.4一般结构及结构思路

--- 1-基本结构及基本数据类型/main/structure.go
+++ 1-基本结构及基本数据类型/main/structure.go
@@ -2,7 +2,7 @@
  * @Author: YangJijun yangjijuna@gmail.com
  * @Date: 2023-03-15 05:43:00
  * @LastEditors: YangJijun yangjijuna@gmail.com
- * @LastEditTime: 2023-03-15 06:16:04
+ * @LastEditTime: 2023-03-15 06:59:48
  * @FilePath: \golang_study\1-基本结构及基本数据类型\structure.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@
 // 	1.按顺序导入所有呗main引用的其他包，然后每个包执行流程如下：
 // 	2.若改包又导入有其他包，则从第一步开始递归，但每个包只会被导入一次
 // 	3.后以相反的顺序在每个包中初始常量与变量，若含有init函数则调用该函数
-// 	4.完成以上步骤后，main也执行同样的过程，最后调用main函数执行程序
+// 	4.完成以上步骤后，main也执行同样的过程，最后调用main函数执行程序。
 package main
 
 import (
